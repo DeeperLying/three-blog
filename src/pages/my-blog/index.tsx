f55ed36ad5d58912df8d7cd281d6326790417361
李应 <2439175930@qq.com>
2023-07-02 01:28:09 +0800

    Feat: update article UI

--- src/pages/my-blog/index.tsx
+++ src/pages/my-blog/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: Lee
  * @Date: 2023-06-23 19:53:51
- * @LastEditTime: 2023-07-02 00:41:52
+ * @LastEditTime: 2023-07-02 01:27:28
  * @LastEditors: Lee
  */
 import React, { useEffect, useState } from 'react'
@@ -13,6 +13,9 @@ import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
+import AccessTimeIcon from '@mui/icons-material/AccessTime'
+import EditNoteIcon from '@mui/icons-material/EditNote'
+import moment from 'moment'
 
 import { serviceGetUserArticleList } from 'src/https/my-blog'
 
@@ -21,7 +24,7 @@ import styles from './index.module.scss'
 const MyBlog = () => {
   const navigate = useNavigate()
   const userInfo = getUserInfo()
-  const [userArticleList, setUserArticleList] = useState<[any]>([{}])
+  const [userArticleList, setUserArticleList] = useState<[any]>()
 
   useEffect(() => {
     handleGetUserArticleList()
@@ -43,33 +46,52 @@ const MyBlog = () => {
 
   return (
     <div className={styles.main}>
-      <Link to={'/create'}>去发表文章</Link>
-      <div>
-        {userArticleList?.length
-          ? userArticleList.map((item) => (
-              <Card key={item.id}>
-                {/* <CardMedia
+      <div className={styles.main_articles}>
+        {userArticleList?.length ? (
+          userArticleList.map((item) => (
+            <Card key={item.id} sx={{ borderRadius: '15px' }}>
+              {/* <CardMedia
                   sx={{ height: 140 }}
                   image='/static/images/cards/contemplative-reptile.jpg'
                   title='green iguana'
                 /> */}
-                <CardContent sx={{ paddingBottom: 0 }}>
-                  <Typography gutterBottom variant='h5' component='div'>
-                    {item.title}
-                  </Typography>
-                  <Typography variant='body2' color='text.secondary'>
-                    {item.introduction}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size='small'>分享</Button>
-                  <Button size='small' onClick={() => navigate('/article/' + item.id)}>
-                    阅读
-                  </Button>
-                </CardActions>
-              </Card>
-            ))
-          : null}
+              <CardContent sx={{ paddingBottom: 0 }}>
+                <Typography gutterBottom variant='h5' component='div'>
+                  {item.title}
+                </Typography>
+                <Typography component='p' className={styles.time}>
+                  <AccessTimeIcon sx={{ width: '1.2rem' }} />
+                  {moment(item.create_time).format('MMMM Do YYYY, h:mm:ss a')}
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                  {item.introduction}
+                </Typography>
+              </CardContent>
+              <CardActions sx={{ justifyContent: 'flex-end' }}>
+                <Button size='small'>分享</Button>
+                <Button size='small' onClick={() => navigate('/article/' + item.id)}>
+                  阅读
+                </Button>
+              </CardActions>
+            </Card>
+          ))
+        ) : (
+          <div className={styles.noArticle}>
+            <EditNoteIcon sx={{ fontSize: '10rem', color: '#007FFF' }} />
+            还没有发布文章，快去发表文章吧。
+          </div>
+        )}
+      </div>
+
+      <div className={styles.create}>
+        <Button
+          variant='outlined'
+          size='large'
+          onClick={() => navigate('/create')}
+          sx={{ background: '#fff' }}
+        >
+          去发表文章
+        </Button>
       </div>
     </div>
   )
